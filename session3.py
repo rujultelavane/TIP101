e991314639e598d437532a902f3d4e5fc3c9d6d9
Rujul Telavane <rstelavane@gmail.com>
2026-03-03 21:56:49 -0500

    working on practice problems with unit 3 -- strings

--- session3.py
+++ session3.py
@@ -41,3 +41,81 @@ def group_anagrams(words):
 words = ["eat", "tea", "tan", "ate", "nat", "bat"]
 #expected: [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
 print(group_anagrams(words))
+
+############# BREAKOUT ROOM PRACTICE -- UNIT 3 #############
+#1.
+def match_made(dictionary):
+    for key, value in dictionary.items():
+        print( f"{key} and {value} are a perfect match.")
+
+names ={
+    'Peanut butter': 'Jelly',
+    'Spongebob' : "Patrick",
+    'Ash':'Pikachu'
+}
+
+match_made(names)
+
+#2. 
+def remove_char(s, n):
+    s = s[:n] + s[n+1:]
+    print(s)
+    
+remove_char("typpo", 2)
+
+#3.
+def vowel_count(s):
+    vow="aeiouyAEIOUY"
+    count=0
+    for char in s:
+        if char in vow:
+            count+=1
+    return count
+
+my_str = "hello world"
+my_str2 = "aAaAaAaAAA"
+my_str3 = "ths strng s mssng vwls"
+print(vowel_count(my_str))
+print(vowel_count(my_str2))
+print(vowel_count(my_str3))
+
+#4. 
+def reverse_sentence(sentence):
+    lst = sentence.split(" ")
+    lst.reverse()
+    return " ".join(lst)
+
+sentence = "I solemnly swear I am up to no good"
+print(reverse_sentence(sentence))
+
+#5.
+def compress_string(my_str):
+    stack = []
+    res = ""
+    for ch in my_str:
+        if ch in stack or len(stack)==0:
+            stack.append(ch)
+        else:
+            res += f"{stack.pop()}{len(stack)+1}"
+            stack = []
+            stack.append(ch)
+    for ch in stack:
+        res += f"{stack.pop()}{len(stack)+1}"
+    print(res)
+    
+compress_string("aaaaabbcccd")
+
+#6.
+def find_the_needle(haystack, needle):
+    if needle in haystack:
+        return haystack.index(needle)
+    else:
+        return -1
+
+haystack = "tobeornottobe"
+needle = "be"
+print(find_the_needle(haystack, needle))
+
+haystack2 = "leetcode"
+needle2 = "leeto"
+print(find_the_needle(haystack2, needle2))

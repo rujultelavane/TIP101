3b599b5df4e6fb8136551239c8ca8df63fc93224
Rujul Telavane <rstelavane@gmail.com>
2026-03-24 20:53:55 -0400

    linkedlist lesson 1

--- session9.py
+++ session9.py
@@ -1,9 +1,13 @@
 # ------------------------- linked list lecture 1 -------------------------
+
+#OOP
 '''
-#class - fundamentl to oop
-# - help in modeling real-world entities, making code reusable, organizing complex programs
-#instantiating -- making an instance of a class
-#making a class:
+class - fundamentl to oop
+- help in modeling real-world entities, making code reusable, organizing complex programs
+instantiating -- making an instance of a class
+making a class:
+'''
+
 '''
 class MyClass:
     def __init__(self, param1, param2): #initializes -- NECESSARY
@@ -14,3 +18,33 @@ class MyClass:
 '''
 
 #LINKED LISTS
+'''
+- arrays dont dynamically grow, linkedlists can
+'''
+
+#create node class 
+class Node:
+    def __init__(self, value, value2=None): #value2 is optional
+        self.value = value
+        self.next = None
+
+a = Node(2)
+b = Node(3)
+c = Node(4)
+d = Node(7)
+e = Node(6)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+#iterate thru linked list
+curr = a #pointer
+while(curr != None):
+    if(curr.value == 7):
+        print("lucky!")
+    curr = curr.next
+
+b = None
+print(a.value)
+print(b.value)
